73fe8f5889b4e1f4d513f35e6f2d0286d67a5eb8
parsley001 <dubhe.kai1@gmail.com>
2023-03-28 12:48:28 +0900

    交流ロボコン本番で使用したコード

--- AC2023B_foot/foot.cpp
+++ AC2023B_foot/foot.cpp
@@ -17,7 +17,7 @@ static char stop     = 0x80;//モーター止める
 //12V制御
 DigitalOut sig(PC_12);//12v緊急停止
 DigitalIn  sta(PC_10);//12V検知
-DigitalOut air1(D12), air2(D11);//エア尻(PA_6),(PA_7)
+DigitalOut air1(D11), air2(D12);//エア尻(PA_7),(PA_6)
 DigitalOut red(D6),green(D7),blue(D5);//LED(PB_10),(PA_8),(PB_4)
 
 //通信
@@ -61,10 +61,20 @@ bool Lst_ue;         //Lスティック上
 bool Lst_migi;       //Lスティック右
 bool Lst_sita;       //Lスティック下
 bool Lst_hidari;     //Lスティック左
+
+bool Rst_ue;         //Rスティック上
+bool Rst_migi;       //Rスティック右
+bool Rst_sita;       //Rスティック下
+bool Rst_hidari;     //Rスティック左
 //
 
+//ボタン同時押し
+bool L2R2;            //L2R2;
+bool L2R2button_ue;   //L2R2button_ue;
+bool L2R2button_sita; //L2R2button_sita;
+bool L1Lst_hidari;    //L1Lst_hidari;
+bool R1Lst_migi;      //R1Lst_migi;
 
-char power;  //////使ってない//////
 
 int  res = 0;  //シリアル通信を受信で１
 char data;     //シリアル通信で送られたデータ
@@ -74,7 +84,7 @@ char data;     //シリアル通信で送られたデータ
 void send(char add, char data);//モーターを回す
 void send_all(char d_mu, char d_ms, char d_hs, char d_hu);//モーターを4つ回す
 void receive_data(void);//シリアル通信受け取り（PS3入力受け取り）
-int iswood(AnalogIn pr_left, AnalogIn pr_right);
+int iswood(AnalogIn pr_left, AnalogIn pr_right, char fdpower);
 
 
 int main(){
@@ -88,8 +98,6 @@ int main(){
     while (true) {
         receive_data();//データ取得
 
-        // printf("%d %d  %d %d %d %d  %d %d %d %d  %d %d %d %d  %d %d %d %d\n", start, select,  button_sankaku, button_maru, button_batu, button_sikaku,  button_ue, button_migi, button_sita, button_hidari,  Lst_ue, Lst_migi, Lst_sita, Lst_hidari,  R1, R2, L1, L2);
-        printf("four %.3f   one %.3f   zero %.3f   three %.3f   two %.3f   five %.3f\n", pr4*5, pr1*5, pr0*5, pr3*5, pr2*5, pr5*5);
         //緊急停止
         if(select){
             sig = 1;
@@ -105,22 +113,33 @@ int main(){
 
 //自動障害物越えプログラム
         if(button_sankaku){
-            if(iswood(pr5, pr4)){
+            if(iswood(pr5, pr4, 0x88)){
                 air1 = 0;//前方輪上げ
-                if(iswood(pr2, pr1)){
-                    air1 = 1;
-                    air2 = 0;
-                    if (iswood(pr3, pr0)) {
-                        air2 = 1;
-                    }
-                }
-            }
-        }else if(L2 && R2){
-            if(button_ue){
+                send_all(256-0x98, 256-0x98, 0x94, 0x94);//直進
+                wait_us(1300*1000);
                 air1 = 1;
-            }if(button_sita){
+                send_all(256-0x9a, 256-0x9a, 0x94, 0x94);
+                wait_us(500*1000);
+                air2 = 0;
+                while(!(L2R2) && !select){
+                    if((pr3*5)<wd || (pr0*5)<wd) break;
+                    send_all(256-0x9a, 256-0x9a, 0x94, 0x94);
+                }
                 air2 = 1;
+                
+                
+                // if(iswood(pr2, pr1, 0x94)){
+                //     air1 = 1;
+                //     air2 = 0;
+                //     if (iswood(pr3, pr0, 0x94)) {
+                //         air2 = 1;
+                //     }
+                // }
             }
+        }else if(L2R2button_ue){
+            air1 = 1;
+        }else if(L2R2button_sita){
+            air2 = 1;
         }else if(button_ue){
             air1 = 0;
         }else if(button_sita){
@@ -135,53 +154,50 @@ int main(){
 //旋回
         //機体左に旋回
         if(L1 && !moved_asimawari){
-            //つよい旋回
-            if(Lst_hidari){
-                send_all(256-_forward, _forward, back, 256-back);
-                moved_asimawari = 1;
             //よわい旋回
-            }else{
-                send_all(256-turnfd, turnfd, turnbk, 256-turnbk);
-                moved_asimawari = 1;
-            }
+            send_all(256-turnfd, turnfd, turnbk, 256-turnbk);
+            moved_asimawari = 1;
+        }else if(Rst_hidari){
+            //つよい旋回
+            send_all(256-_forward, _forward, back, 256-back);
+            moved_asimawari = 1;
         }
 
         //機体右に旋回
         if(R1 && !moved_asimawari){
-            //つよい旋回
-            if(Lst_migi){
-                send_all(256-back, back, _forward, 256-_forward);
-                moved_asimawari = 1;
             //よわい旋回
-            }else{
-                send_all(256-turnbk, turnbk, turnfd, 256-turnfd);
-                moved_asimawari = 1;
-            }
+            send_all(256-turnbk, turnbk, turnfd, 256-turnfd);
+            moved_asimawari = 1;
+        }else if(Rst_migi){
+            //つよい旋回
+            send_all(256-back, back, _forward, 256-_forward);
+            moved_asimawari = 1;
         }
+        
 
 
 //移動
         //前移動
         if(Lst_ue && !moved_asimawari){
-            send_all(256-_forward, _forward, _forward, 256-_forward);
+            send_all(256-_forward, 256-_forward, _forward, _forward);
             moved_asimawari = 1;
         }
 
         //右移動
         if(Lst_migi && !moved_asimawari){
-            send_all(256-back, _forward, back, 256-_forward);
+            send_all(256-back, 256-_forward, back, _forward);
             moved_asimawari = 1;
         }
 
         //左移動
         if(Lst_hidari && !moved_asimawari){
-            send_all(256-_forward, back, _forward, 256-back);
+            send_all(256-_forward, 256-back, _forward, back);
             moved_asimawari = 1;
         }
 
         //後移動
         if(Lst_sita && !moved_asimawari){
-            send_all(256-back, back, back, 256-back);
+            send_all(256-back, 256-back, back, back);
             moved_asimawari = 1;
         }
 
@@ -191,17 +207,19 @@ int main(){
             send_all(stop, stop, stop, stop); 
         }
 
+        // printf("%d %d  %d %d %d %d  %d %d %d %d  %d %d %d %d  %d %d %d %d  %d %d %d %d   %d %d %d %d %d\n", start, select,   button_sankaku, button_maru, button_batu, button_sikaku,   button_ue, button_migi, button_sita, button_hidari,   Lst_ue, Lst_migi, Lst_sita, Lst_hidari,   Rst_ue, Rst_migi, Rst_sita, Rst_hidari,   R1, R2, L1, L2,   L2R2, L2R2button_ue, L2R2button_sita, L1Lst_hidari, R1Lst_migi);
+        printf("four %.3f   one %.3f   zero %.3f   three %.3f   two %.3f   five %.3f\n", pr4*5, pr1*5, pr0*5, pr3*5, pr2*5, pr5*5);
     }//while
 }//main
 
 
 
 void send(char address, char data){
-    wait_us(15000);//send_allに移動すべきでは？
     i2c.start();
     i2c.write(address);
     i2c.write(data);
     i2c.stop();
+    ThisThread::sleep_for(1ms);
 }
 
 void send_all(char d_mu, char d_ms, char d_hs, char d_hu){
@@ -209,6 +227,7 @@ void send_all(char d_mu, char d_ms, char d_hs, char d_hu){
     send(MIGI_SITA, d_ms);
     send(HIDARI_SITA, d_hs);
     send(HIDARI_UE, d_hu);
+    ThisThread::sleep_for(50ms);
 }
 
 //nucleoからのデータ受け取り
@@ -217,29 +236,39 @@ void receive_data(void){
     // printf("%c\n", data);
 
     if(res == 1){
-        if(data == '0') select = 1;         else select = 0;
-        if(data == '1') start = 1;          else start = 0;
+        if(data == '0') select = 1;          else select = 0;
+        if(data == '1') start = 1;           else start = 0;
 
-        if(data == '2') button_sankaku = 1; else button_sankaku = 0;
-        if(data == '3') button_maru = 1;    else button_maru = 0;
-        if(data == '4') button_batu = 1;    else button_batu = 0;
-        if(data == '5') button_sikaku = 1;  else button_sikaku = 0;
+        if(data == 'B') L2R2button_ue = 1;   else L2R2button_ue = 0;   
+        if(data == 'C') L2R2button_sita = 1; else L2R2button_sita = 0; 
+        if(data == 'D') L1Lst_hidari = 1;    else L1Lst_hidari = 0;    
+        if(data == 'E') R1Lst_migi = 1;      else R1Lst_migi = 0;      
+        if(data == 'A') L2R2 = 1;            else L2R2 = 0;            
 
-        if(data == '6') button_ue = 1;      else button_ue = 0;
-        if(data == '7') button_migi = 1;    else button_migi = 0;
-        if(data == '8') button_sita = 1;    else button_sita = 0;
-        if(data == '9') button_hidari = 1;  else button_hidari = 0;
+        if(data == '2') button_sankaku = 1;  else button_sankaku = 0;
+        if(data == '3') button_maru = 1;     else button_maru = 0;
+        if(data == '4') button_batu = 1;     else button_batu = 0;
+        if(data == '5') button_sikaku = 1;   else button_sikaku = 0;
 
-        if(data == 'a') R1 = 1;             else R1 = 0;
-        if(data == 'b') R2 = 1;             else R2 = 0;
-        if(data == 'c') L1 = 1;             else L1 = 0;
-        if(data == 'd') L2 = 1;             else L2 = 0;
+        if(data == '6') button_ue = 1;       else button_ue = 0;
+        if(data == '7') button_migi = 1;     else button_migi = 0;
+        if(data == '8') button_sita = 1;     else button_sita = 0;
+        if(data == '9') button_hidari = 1;   else button_hidari = 0;
 
-        if(data == 'e') Lst_ue = 1;         else Lst_ue = 0;
-        if(data == 'f') Lst_migi = 1;       else Lst_migi = 0;
-        if(data == 'g') Lst_sita = 1;       else Lst_sita = 0;
-        if(data == 'h') Lst_hidari = 1;     else Lst_hidari = 0;
+        if(data == 'a') R1 = 1;              else R1 = 0;
+        if(data == 'b') R2 = 1;              else R2 = 0;
+        if(data == 'c') L1 = 1;              else L1 = 0;
+        if(data == 'd') L2 = 1;              else L2 = 0;
 
+        if(data == 'e') Lst_ue = 1;          else Lst_ue = 0;
+        if(data == 'f') Lst_migi = 1;        else Lst_migi = 0;
+        if(data == 'g') Lst_sita = 1;        else Lst_sita = 0;
+        if(data == 'h') Lst_hidari = 1;      else Lst_hidari = 0;
+
+        if(data == 'i') Rst_ue = 1;          else Rst_ue = 0;
+        if(data == 'j') Rst_migi = 1;        else Rst_migi = 0;
+        if(data == 'k') Rst_sita = 1;        else Rst_sita = 0;
+        if(data == 'l') Rst_hidari = 1;      else Rst_hidari = 0;
     }
 }
 
@@ -252,17 +281,20 @@ void receive_data(void){
    *両方のフォトリフレクタが反応→１を返し終了
    *R2とL2を同時押しもしくはselectを押すと０を返し終了
 */
-int iswood(AnalogIn pr_left, AnalogIn pr_right){
+int iswood(AnalogIn pr_left, AnalogIn pr_right, char fdpower){
     //R2L2同時押しでキャンセル
-    while(!(R2 && L2) && !select){
+    while(!(L2R2) && !select){
         if((pr_right*5)<wd && (pr_left*5)<wd){//前のフォトリフレクタがどちらも検知したら
             return 1;
         }else if((pr_left*5)<wd){//左前だけ検知
-            send_all(stop, turnfd, turnbk, stop);//前方軸に反時計回り
+            // send_all(256-0x76, 256-0x76, 0x76, 0x76);
+            send_all(256-stop, stop, 0x72, 256-0x72);//前方軸に反時計回り
         }else if((pr_right*5)<wd){//右前だけ検知
-            send_all(stop, turnbk, turnfd, stop);//前方軸に時計回り
+            // send_all(256-0x76, 256-0x76, 0x76, 0x76);
+            send_all(256-0x72, 0x72, stop, 256-stop);//前方軸に時計回り
         }else{
-            send_all(256-turnfd, turnfd, turnfd, 256-turnfd);//直進
+            // send_all(0x80, 0x80, 0x80, 0x80);
+            send_all(256-fdpower, 256-fdpower, fdpower, fdpower);//直進
         }
         receive_data();
         printf("four %.3f   one %.3f   zero %.3f   three %.3f   two %.3f   five %.3f\n", pr4*5, pr1*5, pr0*5, pr3*5, pr2*5, pr5*5);
--- AC2023B_tray/tray.cpp
+++ AC2023B_tray/tray.cpp
@@ -25,6 +25,11 @@ bool Lst_migi;       //Lスティック右
 bool Lst_sita;       //Lスティック下
 bool Lst_hidari;     //Lスティック左
 
+bool Rst_ue;         //Rスティック上
+bool Rst_migi;       //Rスティック右
+bool Rst_sita;       //Rスティック下
+bool Rst_hidari;     //Rスティック左
+
 float Rangle;        // ジョイコン　右　角度
 float Langle;        // ジョイコン　左　角度
 
@@ -57,15 +62,17 @@ int main(){
     tuusin.format(8, BufferedSerial::None, 1);//シリアル通信設定(bits, parity, stopbit)
 
     int servo_start=560, servo_end=2200;//サーボの位置
-    servo1.pulsewidth_us(servo_start);//サーボ初期位置
-    servo2.pulsewidth_us(servo_start);
-    servo3.pulsewidth_us(servo_start);
+    servo1.pulsewidth_us(servo_end);//サーボ初期位置
+    servo2.pulsewidth_us(servo_end);
+    servo3.pulsewidth_us(servo_end);
 
     while (true){
         get_data();
 
 
 //スティック処理
+
+        //Lスティック
         if(Lx>60 || Lx<-60 || Ly>60 || Ly<-60){//スティックが最大限倒れている
             if(Langle<45 && Langle>=-45)   Lst_migi = 1;   else Lst_migi = 0;
             if(Langle<135 && Langle>=45)   Lst_ue = 1;     else Lst_ue = 0;
@@ -78,8 +85,20 @@ int main(){
             Lst_sita = 0;
         }
 
+        //Rスティック
+        if(Rx>60 || Rx<-60 || Ry>60 || Ry<-60){//スティックが最大限倒れている
+            if(Rangle<45 && Rangle>=-45){   Rst_migi = 1;  printf("migi\n"); }else Rst_migi = 0;
+            if(Rangle<135 && Rangle>=45){   Rst_ue = 1;    printf("ue\n"); }else Rst_ue = 0;
+            if(Rangle<-135 || Rangle>=135){ Rst_hidari = 1;printf("hidari\n"); }else Rst_hidari = 0;
+            if(Rangle<-45 && Rangle>=-135){ Rst_sita = 1;  printf("sita\n"); }else Rst_sita = 0;
+        }else{
+            Rst_migi = 0;
+            Rst_ue = 0;
+            Rst_hidari = 0;
+            Rst_sita = 0;
+        }
 
-        printf("%d %d  %d %d %d %d  %d %d %d %d  %7.2f %3d %3d  %d %d %d %d  %d %d %d %d\n", start, select,  button_sankaku, button_maru, button_batu, button_sikaku,  button_ue, button_migi, button_sita, button_hidari,  Langle, Lx, Ly,  Lst_ue, Lst_migi, Lst_sita, Lst_hidari,  R1, R2, L1, L2);
+        printf("%d %d  %d %d %d %d  %d %d %d %d  %7.2f %3d %3d  %7.2f %3d %3d  %d %d %d %d  %d %d %d %d\n", start, select,   button_sankaku, button_maru, button_batu, button_sikaku,   button_ue, button_migi, button_sita, button_hidari,   Langle, Lx, Ly,   Rangle, Rx, Ry,   Lst_ue, Lst_migi, Lst_sita, Lst_hidari,   R1, R2, L1, L2);
 
         send_data();
 
@@ -149,10 +168,17 @@ void get_data(void){
 //もう一方のnucleoにデータ送信
 void send_data(void){
     // printf("maru:%d batu:%d\n",button_maru,button_batu);
-    if(select || start || button_sankaku || button_maru || button_batu || button_sikaku || button_ue || button_migi || button_sita || button_hidari || Lst_ue || Lst_migi || Lst_sita || Lst_hidari || R1 || R2 || L1 || L2){
+    if(select || start || button_sankaku || button_maru || button_batu || button_sikaku || button_ue || button_migi || button_sita || button_hidari || Lst_ue || Lst_migi || Lst_sita || Lst_hidari || Rst_ue || Rst_migi || Rst_sita || Rst_hidari || R1 || R2 || L1 || L2){
         printf("hoge\n");
+//順番変えると操作優先順位が変わる
         if(select)         tuusin.write("0", 1); //else tuusin.write("000", 1);
         if(start)          tuusin.write("1", 1); //else tuusin.write("001", 1);
+
+        if(L2 && R2 && button_ue)   tuusin.write("B", 1);
+        if(L2 && R2 && button_sita) tuusin.write("C", 1);
+        if(L1 && Lst_hidari)        tuusin.write("D", 1);
+        if(R1 && Lst_migi)          tuusin.write("E", 1);
+        if(L2 && R2)                tuusin.write("A", 1);
 
         if(button_sankaku) tuusin.write("2", 1); //else tuusin.write("002", 1);
         if(button_maru)    tuusin.write("3", 1); //else tuusin.write("003", 1);
@@ -164,16 +190,21 @@ void send_data(void){
         if(button_sita)    tuusin.write("8", 1); //else tuusin.write("i", 1);
         if(button_hidari)  tuusin.write("9", 1); //else tuusin.write("j", 1);
 
-//順番変えると操作優先順位変わる？
         if(R1)             tuusin.write("a", 1); //else tuusin.write("k", 1);
         if(R2)             tuusin.write("b", 1); //else tuusin.write("l", 1);
         if(L1)             tuusin.write("c", 1); //else tuusin.write("m", 1);
         if(L2)             tuusin.write("d", 1); //else tuusin.write("n", 1); 
 
+        if(Rst_ue)         tuusin.write("i", 1);
+        if(Rst_migi)       tuusin.write("j", 1);
+        if(Rst_sita)       tuusin.write("k", 1);
+        if(Rst_hidari)     tuusin.write("l", 1);
+
         if(Lst_ue)         tuusin.write("e", 1);
         if(Lst_migi)       tuusin.write("f", 1);
         if(Lst_sita)       tuusin.write("g", 1);
         if(Lst_hidari)     tuusin.write("h", 1);
+
 
     }else {
         tuusin.write("z", 1);
